33c116bccb4ab049c38b8064b295430df21520d4
Zero <110248067+Z3R00P@users.noreply.github.com>
2024-09-21 16:43:16 -0400

    Update client.py

--- gemini_api/client.py
+++ gemini_api/client.py
@@ -3,7 +3,7 @@ import os
 
 def get_car_ai_bio(model, brand, year):
     prompt = '''Me mostre uma descrição de venda para o carro {} {} {} em apenas 250 caracteres. Fale coisas específicas desse modelo.'''.format(brand, model, year)
-    genai.configure(api_key='AIzaSyDK-Wlr7w-uPmKBwtDlsrQNXkH2hD0QRos')
+    genai.configure(api_key=' ')
     model = genai.GenerativeModel("gemini-1.5-flash")
     response = model.generate_content(prompt)
     return response.text
